186fb2d9be2d7fcc6f6417f2b8a1fe94fe2d046c
Kehinde Orilogbon <archkenny@yahoo.com>
2020-06-14 16:04:01 +0100

    set up header

---
+++ src/components/header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles.scss";
+
+const Header = () => {
+	return (
+		<header>
+			<div className="wrap">
+				<div className="logo">
+					<img
+						src="https://via.placeholder.com/150x50/000/fff?text=Logo"
+						alt=""
+					/>
+				</div>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import "./app.scss";
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
