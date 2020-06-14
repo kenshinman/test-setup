b151a7c042bcd1d8d799313a3f8d1d88604ddda6
Kehinde Orilogbon <archkenny@yahoo.com>
2020-06-15 00:24:40 +0100

    implemented redux  initialization

---
+++ src/createStore.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+
+export const middlewares = [thunk];
+
+export const createStoreWithMiddleware = applyMiddleware(...middlewares)(
+	createStore
+);
+
+export const store = createStoreWithMiddleware(rootReducer);
